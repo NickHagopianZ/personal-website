d86b4fd9c85bbd338a4d3c4d1a57d2a43c3326ad
NickHagopianZ <22013234+NickHagopianZ@users.noreply.github.com>
2022-10-02 10:49:53 -0500

    use no-reply email

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,17 @@ export class HomeComponent implements OnInit {
   constructor() { }
   imageUrl: string  = `assets/profileSquareOptimized.jpg?${Date.now()}`;
 
+  time = new Date();
+  rxTime = new Date();
+  intervalId;
+
   ngOnInit() {
+    // Using Basic Interval
+    // const epochToday = (new Date).setHours(0,0,0,0).getTime();
+
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
   }
 
 }
